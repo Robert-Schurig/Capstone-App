c4502ed1df5ba184d75f7a2058ef16eaf95be365
Robert Schurig <robertschurig@yahoo.com>
2022-11-30 14:36:22 +0100

    Even more corrections

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import GlobalStyles from "../styles/GlobalStyles";
-import Navbar from "../components/Navbar";
 
 function MyApp({Component, pageProps}) {
   return (
     <>
       <GlobalStyles />
       <Component {...pageProps} />
-      <Navbar />
     </>
   );
 }

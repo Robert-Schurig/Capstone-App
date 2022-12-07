b8d8ea5f5505e01905dd443a4bce5be9f3923e6a
Robert Schurig <robertschurig@yahoo.com>
2022-12-07 12:15:48 +0100

    Beginning a like functionality

--- components/imageCard.js
+++ components/imageCard.js
@@ -1,11 +1,23 @@
 import {useState, useEffect} from "react";
 import Image from "next/image";
 import styled from "styled-components";
+import deleteIcon from "../public/deleteIcon.svg";
+import likeIcon from "../public/likeIcon.svg";
 
 export default function ImageCard() {
   const [dataArray, setDataArray] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [displayImage, setDisplayImage] = useState(false);
+  const [favorite, setFavorite] = useState([]);
+
+  function addFavorite(index) {
+    if (favorite.includes(index)) {
+      const newFavorite = favorite.filter(favIndex => favIndex !== index);
+      setFavorite(newFavorite);
+    } else {
+      setFavorite([...favorite, index]);
+    }
+  }
 
   useEffect(() => {
     setLoading(true);
@@ -19,7 +31,6 @@ export default function ImageCard() {
         setDisplayImage(randomizeImage(serverData.items));
       });
   }, []);
-  console.log(dataArray);
 
   function randomizeImage(dataArray) {
     const randomIndex = Math.round(Math.random() * dataArray.length);
@@ -30,7 +41,7 @@ export default function ImageCard() {
   if (isLoading) return <p>Loading...</p>;
   if (!dataArray) return <p>No data</p>;
   return (
-    <>
+    <main>
       {displayImage ? (
         <>
           <ImageContainer>
@@ -50,12 +61,50 @@ export default function ImageCard() {
       ) : (
         ""
       )}
-    </>
+
+      <ButtonContainer>
+        <Button>
+          <Image
+            src={deleteIcon}
+            alt="Delete Icon"
+            height={80}
+            width={80}
+            objectFit="contain"
+          />
+        </Button>
+
+        <Button onClick={() => addFavorite(favorite)}>
+          <Image
+            src={likeIcon}
+            alt="Like Icon"
+            height={80}
+            width={80}
+            objectFit="contain"
+          />
+        </Button>
+      </ButtonContainer>
+    </main>
   );
 }
+
+const Main = styled.main`
+  display: flex;
+`;
 
 const ImageContainer = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  padding-top: 60px;
+`;
+
+const Button = styled.button`
+  border: none;
+  background: none;
 `;
--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -1,3 +1,39 @@
-export default function Favorites() {
-  <p>Hier findest du deine gespeicherten Bilder.</p>;
+import styled from "styled-components";
+import {useState, useEffect} from "react";
+import Image from "next/legacy/image";
+
+export default function Favorites(favorite) {
+  const [favImages, setFavImages] = useState([]);
+
+  useEffect(() => {
+    if (favorite.length === 0) {
+      setFavImages([]);
+    } else {
+      fetch(
+        "https://api.europeana.eu/record/v2/search.json?media=true&profile=minimal&query=Painting&theme=art&reusability=open&qf=IMAGE_COLOUR:%22TRUE%22&rows=100&wskey=hanozzle"
+      )
+        .then(response => response.json())
+        .then(favImages => setFavImages(favImages));
+      console.log(response);
+    }
+  }, [favorite]);
+
+  return (
+    <div>
+      {favImages.map(favImage => {
+        return (
+          <article key={favImage.index}>
+            <p>{favImage.title}</p>
+            <Image
+              key={favImage.index}
+              src={favImage.edmPreview[0]}
+              alt={favImage.title[0]}
+              fill="true"
+              objectFit="contain"
+            />
+          </article>
+        );
+      })}
+    </div>
+  );
 }

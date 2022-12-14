6d1833e2a0dfd4fa2c009aa78c20d54b8270c85a
Robert Schurig <robertschurig@yahoo.com>
2022-12-14 10:25:50 +0100

    One more try

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import GlobalStyles from "../styles/GlobalStyles";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import {ImageContextProvider} from "../components/ImageContext";
 
 function MyApp({Component, pageProps}) {

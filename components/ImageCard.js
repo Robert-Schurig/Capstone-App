0dc6c7d55130e6fd4900ed3840e0f65da621f0af
Robert Schurig <robertschurig@yahoo.com>
2022-12-15 12:25:38 +0100

    Finished the like-dislike-function

--- components/ImageCard.js
+++ components/ImageCard.js
@@ -5,13 +5,16 @@ import likeIcon from "../public/likeIcon.svg";
 import likeIconFilled from "../public/likeIconFilled.svg";
 import {useContext} from "react";
 import {ImageContext} from "./ImageContext";
+import css from "styled-jsx/css";
 
 export default function ImageCard() {
   const [dataArray, setDataArray] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [displayImage, setDisplayImage] = useState(null);
-  const {addFavorite} = useContext(ImageContext);
   const [reload, setReload] = useState(true);
+  const [random, setRandom] = useState(0);
+  const [clickedButton, setClickedButton] = useState(false);
+  const {addFavorite} = useContext(ImageContext);
 
   useEffect(() => {
     setLoading(true);
@@ -24,7 +27,8 @@ export default function ImageCard() {
           const serverData = await response.json();
           setDataArray(serverData.items);
           setLoading(false);
-          setDisplayImage(randomizeImage(serverData.items));
+          // setDisplayImage(randomizeImage(serverData.items));
+          setReload(!reload);
         } else {
           throw new Error(
             "Fetch fehlgeschlagen mit Status: ${response.status}"
@@ -36,13 +40,21 @@ export default function ImageCard() {
       }
     };
     getImages();
+  }, []);
+
+  useEffect(() => {
+    setRandom(Math.floor(Math.random() * dataArray.length));
   }, [reload]);
 
-  function randomizeImage(dataArray) {
-    const randomIndex = Math.round(Math.random() * dataArray.length);
-    const randomImage = dataArray[randomIndex];
-    return randomImage;
-  }
+  useEffect(() => {
+    setDisplayImage(dataArray[random]);
+  }, [random]);
+
+  // function randomizeImage(dataArray) {
+  //   const randomIndex = Math.floor(Math.random() * dataArray.length);
+  //   const randomImage = dataArray[randomIndex];
+  //   return randomImage;
+  // }
 
   if (isLoading) return <p>Loading...</p>;
   if (!dataArray) return <p>No data</p>;
@@ -59,17 +71,21 @@ export default function ImageCard() {
               height={300}
             />
           </ImageContainer>
-          <div>
+          <Details>
             <p>{displayImage.title[0]}</p>
             <p>{displayImage.dcCreator}</p>
-          </div>
+          </Details>
         </>
       ) : (
         ""
       )}
 
       <ButtonContainer>
-        <DeleteButton onClick={() => setReload(!reload)}>
+        <ReloadButton
+          onClick={() => {
+            setReload(!reload);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="100"
@@ -81,16 +97,29 @@ export default function ImageCard() {
               d="M12 3c-4.963 0-9 4.038-9 9s4.037 9 9 9s9-4.038 9-9s-4.037-9-9-9zm0 16c-3.859 0-7-3.14-7-7s3.141-7 7-7s7 3.14 7 7s-3.141 7-7 7zm.707-7l2.646-2.646a.502.502 0 0 0 0-.707a.502.502 0 0 0-.707 0L12 11.293L9.354 8.646a.5.5 0 0 0-.707.707L11.293 12l-2.646 2.646a.5.5 0 0 0 .707.708L12 12.707l2.646 2.646a.5.5 0 1 0 .708-.706L12.707 12z"
             />
           </svg>
-        </DeleteButton>
+        </ReloadButton>
 
-        <LikeButton onClick={() => addFavorite(displayImage)}>
-          <Image src={likeIcon} alt="Like Icon" height={80} width={80} />
-          <Image
-            src={likeIconFilled}
-            alt="Like Icon Filled"
-            height={80}
-            width={80}
-          />
+        <LikeButton
+          onClick={() => {
+            addFavorite(displayImage);
+            setReload(!reload);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="85"
+            height="85"
+            viewBox="0 0 48 48"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="4"
+              d="M15 8C8.925 8 4 12.925 4 19c0 11 13 21 20 23.326C31 40 44 30 44 19c0-6.075-4.925-11-11-11c-3.72 0-7.01 1.847-9 4.674A10.987 10.987 0 0 0 15 8Z"
+            />
+          </svg>
         </LikeButton>
       </ButtonContainer>
     </div>
@@ -103,6 +132,11 @@ const ImageContainer = styled.div`
   height: 400px;
 `;
 
+const Details = styled.div`
+  display: block;
+  height: 110px;
+`;
+
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-around;
@@ -110,12 +144,37 @@ const ButtonContainer = styled.div`
   padding-top: 60px;
 `;
 
-const DeleteButton = styled.button`
+const ReloadButton = styled.button`
   border: none;
   background: none;
+
+  path {
+    transition: 0.5s;
+  }
+  transition: 0.5s;
+  :active {
+    transform: scale(0.8);
+    transition: 0.5s;
+    path {
+      fill: darkviolet;
+      transition: 0.5s;
+    }
+  }
 `;
 
 const LikeButton = styled.button`
   border: none;
   background: none;
+  path {
+    transition: 0.5s;
+  }
+  transition: 0.5s;
+  :active {
+    transform: scale(0.8);
+    transition: 0.5s;
+    path {
+      stroke: darkviolet;
+      transition: 0.5s;
+    }
+  }
 `;
